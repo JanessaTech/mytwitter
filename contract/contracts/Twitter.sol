d94cd16e1a8f79180b7dfe0578cb7c090bcd5525
Jane <lvlajzhao1983@gamil.com>
2023-06-26 12:06:12 +0800

    add contract Twitter.sol

--- contract/contracts/Twitter.sol
+++ contract/contracts/Twitter.sol
@@ -1,8 +1,47 @@
 pragma solidity ^0.8.19;
 
+import "./TwitterUser.sol";
+
 // SPDX-License-Identifier: UNLICENSED
 // Perform the basic functions for twitter
-
 contract Twitter {
+    struct tweet {
+        uint timestamp;
+        string tweetMessage;
+    }
+
+    mapping (address => tweet[]) tweets;
+
+    TwitterUser twitterUsers;
     
+    constructor(address _TwitterUsers) {
+        twitterUsers = TwitterUser(_TwitterUsers);
+    }
+
+    event postTracker(address indexed from, string who, string content);
+
+    modifier isValidUser() {
+         require(twitterUsers.checkIfUserExists(msg.sender), "Unregister user");
+         _;
+    }
+
+    function post(string memory content) public isValidUser {
+        tweets[msg.sender].push(tweet(block.timestamp, content));
+        string memory name = twitterUsers.checkUserName(msg.sender);
+        emit postTracker(msg.sender, name, content);
+    }
+
+    function readPost(uint index) public view isValidUser returns(string memory, string memory, uint) {
+        if (index >= tweets[msg.sender].length) {
+            return ("", "", 0);
+        }
+        tweet storage tw = tweets[msg.sender][index];
+        string memory name = twitterUsers.checkUserName(msg.sender);
+
+        return (name, tw.tweetMessage, tw.timestamp);
+    }
+
+    function postNumbers() public view returns(uint) {
+        return tweets[msg.sender].length;
+    }
 }
--- contract/migrations/1_deploy_all_contracts.js
+++ contract/migrations/1_deploy_all_contracts.js
@@ -1,5 +1,8 @@
 const TwitterUser = artifacts.require("TwitterUser");
+const Twitter = artifacts.require("Twitter");
 
 module.exports = function(deployer,  network, accounts) {
-  deployer.deploy(TwitterUser, {from: accounts[0]});
+  deployer.deploy(TwitterUser, {from: accounts[0]}).then(function() {
+    return deployer.deploy(Twitter, TwitterUser.address);
+  });
 }
